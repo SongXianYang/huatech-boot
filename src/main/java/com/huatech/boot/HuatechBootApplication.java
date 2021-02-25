7c97efebd119f3425aa1c9dab6fdd7f0b2ed9c56
SongXianYang <1965569785@qq.com>
2021-02-25 21:07:51 +0800

    Initial commit

---
+++ src/main/java/com/huatech/boot/HuatechBootApplication.java
@@ -0,0 +1,13 @@
+package com.huatech.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuatechBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuatechBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huatech/boot/HuatechBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huatech.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuatechBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
