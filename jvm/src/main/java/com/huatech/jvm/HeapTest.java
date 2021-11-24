676914dea9b5a948959a40c68d976a6f9add476b
SongXianYang <1965569785@qq.com>
2021-11-24 17:16:58 +0800

    提交代码

--- jvm/src/main/java/com/huatech/jvm/HeapTest.java
+++ jvm/src/main/java/com/huatech/jvm/HeapTest.java
@@ -1,9 +1,18 @@
 package com.huatech.jvm;
 
 /**
- * @description: 堆heap
+ * @description: 堆 oom
+ * 调优指令：-Xms1024m -Xmx1024 -XX:+PrintGCDetails
  * @author: SongXY
  * @create: 2021-11-24 16:54
  **/
 public class HeapTest {
+    public static void main(String[] args) {
+        String s = "sxy";
+        while (true) {
+            s += s + s;
+            System.out.println(s);
+        }
+    }
+
 }
